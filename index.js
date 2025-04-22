2253d4c3ed4f3c5bf16427bdd761299c90126dbd
snomiao <snomiao@gmail.com>
2025-04-22 02:51:46 +0000

    fix(main): use 4o
    use 4o

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ for (let subtitleFile of subtitles) {
     for (;;) {
       try {
         completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
+          model: "gpt-4o",
           messages: [
             {
               role: "system",
